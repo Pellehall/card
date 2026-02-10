6935958e5c3b9ee446d7fc93ea748d122e80d682
Pellehall <pelle.hallqvist@nyckelskolan.se>
2026-02-10 12:34:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const card = document.getElementById("card");
+const card = document.querySelector(".card");
 
 document.addEventListener("mousemove", (e) => {
-  const x = (window.innerWidth / 2 - e.clientX) / 20;
-  const y = (window.innerHeight / 2 - e.clientY) / 20;
+  const x = (window.innerWidth / 2 - e.clientX) / 25;
+  const y = (window.innerHeight / 2 - e.clientY) / 25;
 
-  card.style.transform = `rotateY(${-x}deg) rotateX(${y}deg)`;
+  card.style.transform = `
+    rotateY(${-x}deg)
+    rotateX(${y}deg)
+    scale(1.03)
+  `;
 });
 
 document.addEventListener("mouseleave", () => {
-  card.style.transform = "rotateY(0deg) rotateX(0deg)";
+  card.style.transform = "rotateY(0) rotateX(0) scale(1)";
 });
